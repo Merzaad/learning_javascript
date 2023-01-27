892fec22c01848ddd518ab7caa5d2ec893f296bb
merzaad <merzaad@gmail.com>
2023-01-27 08:25:34 +0330

    sync

--- module.js
+++ module.js
@@ -1,12 +1,25 @@
-const closure = () => {
-  let x = 0
+document.getElementById('x').innerHTML = localStorage.getItem('x') || '0'
+
+const state = () => {
+  let x = Number(localStorage.getItem('x')) || 0
+  const sync = () => {
+    document.getElementById('x').innerHTML = x
+    localStorage.setItem('x', x)
+  }
   const increase = () => {
     x += 1
+    sync()
   }
   const get = () => {
     return x
   }
-  return { increase, get }
+  const reset = () => {
+    if (x !== 0) {
+      x = 0
+      sync()
+    }
+  }
+  return { increase, get, reset }
 }
 
-const { increase, get } = closure()
+const { increase, get, reset } = state()
